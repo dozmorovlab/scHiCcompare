1c01ab4a4cb7c5f25cea4a322cdd05b85ec33034
hamy12398 <hamy.12398@gmail.com>
2024-10-04 12:16:33 -0400

    update package

--- 33_scHiCcompare.package.R
+++ 33_scHiCcompare.package.R
@@ -1,6 +1,10 @@
 # Install required packages if you don't have them already
 install.packages("usethis")
 install.packages("devtools")
+install.packages("roxygen2")
+
+library(roxygen2)
+
 
 # Create a package
 usethis::create_package("/Users/mynguyen/Documents/GitHub/scHiCcompare/scHiCcompare")
@@ -10,6 +14,10 @@ usethis::use_package("dplyr") # Adding dplyr to Imports
 usethis::use_package("dplyr", type = "Suggests") # Adding dplyr to Suggests
 usethis::use_package("tidyr")
 usethis::use_package("HiCcompare")
+usethis::use_package("mice")
+usethis::use_package("miceadds")
+usethis::use_package("rstatix")
+usethis::use_package("lattice")
 
 # Add function
 usethis::use_r("scHiC_table")
@@ -17,3 +25,9 @@ usethis::use_r("PseudoBulk_sparse")
 usethis::use_r("RF_pooling_imputation")
 usethis::use_r("BulkHiC_loess")
 usethis::use_r("BulkHiC_compare")
+
+
+# Add doument for function
+usethis::use_roxygen_md()
+roxygen2::roxygenise()
+
