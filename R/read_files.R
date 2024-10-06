67523b8f0ad889b7ad718d63b5defe32dfeb21cd
hamy12398 <hamy.12398@gmail.com>
2024-10-06 03:25:05 -0400

    update package

---
+++ R/read_files.R
@@ -0,0 +1,98 @@
+#' Read Files for scHi-C Dataset
+#'
+#' This function reads single-cell Hi-C data from a specified file path.
+#' It supports two file formats: 'txt' and 'cool'. For 'txt', it reads tab-delimited files and assumes the format contains five columns:
+#' chromosome 1 (chr1), start position 1 (start1), chromosome 2 (chr2), start position 2 (start2), and interaction frequency (IF).
+#' For 'cool', it uses the `HiCcompare` package to transform cooler files to BEDPE format.
+#'
+#' @param file.path The directory path where the data files are stored.
+#' @param cell A character string specifying a cell type (e.g., 'MG', etc).
+#' @param position.dataset A vector of indices specifying the file positions to read from the directory.
+#' @param type The file type, either 'txt' or 'cool'. Default is 'txt'. Each 'txt' file should be in the format of a sparse upper triangular Hi-C matrix, where each row contains the interaction frequency value (IFs) of two interacting regions.
+#' If the 'cool' type is selected, the .cool files are HDF5 containers that store Hi-C data, which will be read using `cooler2bedpe()` from the `HiCcompare` package.
+#' @param txt.sparse.heads.position A vector of four integers specifying the column positions of chromosome, start1, start2, and IF in the 'txt' file.
+#' @param out Output format with options 'sparse' and 'original'. If 'sparse', the sparse upper triangular matrix format is returned. If 'original', the dataset will retain its original structure. Default is 'sparse'.
+#'
+#' @return A list of datasets, where each element corresponds to a dataset from the selected files. If `out` is 'sparse', each dataset element is transformed into sparse matrix format (chr, start1, start2, IF).
+#' If `out` is 'original', the original structure of each single-cell Hi-C dataset is preserved. If the 'cool' type is selected and `out` is 'original', each dataset will retain the structure from `cooler2bedpe()`, containing a list with two items:
+#' \itemize{
+#'   \item "cis": Contains the intra-chromosomal contact matrices, one per chromosome.
+#'   \item "trans": Contains the inter-chromosomal contact matrix.
+#' }
+#'
+#' @details
+#' This function reads single-cell Hi-C data in '.txt' or 'cool' format, with output options of 'sparse' and 'original'. Each input 'txt' file should be in the form of a sparse upper triangular Hi-C matrix, 
+#' storing pair-wise interaction frequencies of loci pairs. The 'txt' dataset should have one column indicating the interaction frequency (IF) of each pair of interacting regions, with tab-separated columns and no row names, column names, or quotes around character strings.
+#' If the 'cool' type is selected, the input .cool files are HDF5 containers that store Hi-C data, which will be read using `cooler2bedpe()` from the `HiCcompare` package. The function can provide output in the dataset's 'original' structure or in 'sparse' matrix format.
+#'
+#' @examples
+#' \dontrun{
+#' # Load MG data folder example
+#' load_example_MGfolder()
+#' datasets <- read_files(file.path = "MGs_example", cell = "MG", position.dataset = c(1, 2, 3), type = "txt", txt.sparse.heads.position = c(1,2,4,5))
+#' }
+#' @import HiCcompare
+#' @export
+
+
+read_files <- function(file.path, cell, position.dataset, type='txt', txt.sparse.heads.position = NULL, out = 'sparse'){
+  # Check if the file path exists
+  if(!dir.exists(file.path)){
+    stop("Error: The specified file path does not exist.")
+  }
+  
+  data_names <- list.files(path = file.path, full.names = TRUE, recursive = TRUE)[position.dataset] 
+  
+  # Check if the selected files are valid
+  if (length(data_names) == 0) {
+    stop("Error: No files found for the specified position.dataset.")
+  }
+  
+  datasets <- list()  # Initialize a list to store the datasets
+  
+  if (type == 'txt') {
+    for(i in 1:length(position.dataset)){
+      dataset <- tryCatch(
+        
+        read.delim(data_names[i]),
+        error = function(e) stop(paste("Error reading file:", data_names[i]))
+      )
+      if(out == 'sparse'){
+        ## Identify head position in sparse matrix format
+        chr.position = txt.sparse.heads.position[1]
+        start1.position = txt.sparse.heads.position[2]
+        start2.position = txt.sparse.heads.position[3]
+        if.position = txt.sparse.heads.position[4]
+        dataset <- dataset[ ,c(chr.position, start1.position, start2.position, if.position)]
+        names(dataset) = c('chr', 'start1','start2')
+      }
+      datasets[[i]] <- dataset  # Store each dataset in the list
+    }
+  } else if (type == 'cool') {
+    if (!requireNamespace("HiCcompare", quietly = TRUE)) {
+      stop("Error: HiCcompare package not installed. Please install the HiCcompare package.")
+    }
+    library(HiCcompare)
+    for(i in 1:length(position.dataset)){
+      dataset <- tryCatch(
+        cooler2bedpe(data_names[i]),
+        error = function(e) stop(paste("Error reading .cool file:", data_names[i]))
+      )
+      if(out == 'sparse'){
+        ## Identify head position in sparse matrix format
+        data_all <- do.call(rbind,dataset$cis)
+        dataset <- data_all[,c('chr1', 'start1', 'start2','IF')]
+        names(dataset) <- c('chr', 'region1', 'region2','IF')
+        row.names(dataset) = NULL
+      }
+      datasets[[i]] <- dataset  # Store each dataset in the list
+    }
+  } else {
+    stop("Error: Unsupported file type. Use 'txt' or 'cool'.")
+  }
+  
+  return(datasets)
+}
+
+
+
--- R/scHiC_table.R
+++ R/scHiC_table.R
@@ -1,124 +1,42 @@
-#' Read Files for scHi-C Dataset
-#'
-#' This function reads single-cell Hi-C data from a specified file path.
-#' It supports two file formats: 'txt' and 'cool'. For 'txt', it reads tab-delimited files and assumes the format contains five columns: 
-#' chromosome 1 (chr1), start position 1 (start1), chromosome 2 (chr2), start position 2 (start2), and interaction frequency (IF).
-#' For 'cool', it uses the `HiCcompare` package to transform cooler files to BEDPE format.
-#'
-#' @param file.path The directory path where the data files are stored. 
-#' @param cell A character string specifying a cell type (eg., 'MG', etc).
-#' @param position.dataset A vector of indices specifying the file positions to read from the directory.
-#' @param type The file type, either 'txt' or 'cool'. Default is 'txt'. Each 'txt; file should be in the format of a sparse upper triangular Hi-C matrix with five columns: 
-#' chromosome 1 (chr1), start position 1 (start1), chromosome 2 (chr2), start position 2 (start2), and interaction frequency (IF). 
-#' If the 'cool' type is selected, the .cool files are HDF5 containers that store Hi-C data, which will be read using `cooler2bedpe()` from the `HiCcompare` package.
-#' @return A list of datasets, where each element corresponds to a dataset from the selected files. If 'txt' type is selected, each element contains a data frame of a sparse upper triangular Hi-C matrix. 
-#' If 'cool' is selected, each element contains a list with two items: 
-#' \itemize{
-#'   \item "cis": Contains the intra-chromosomal contact matrices, one per chromosome.
-#'   \item "trans": Contains the inter-chromosomal contact matrix. 
-#' }
-#' @details
-#' This function input single cell Hi-C data in '.txt' or 'cool' format. Each 'txt; file should be in the form of a sparse upper triangular Hi-C matrix with five columns: 
-#' chromosome 1 (chr1), start genomic position 1 (start1), chromosome 2 (chr2), start genomic position 2 (start2), and interaction frequency (IF), with format of tab-separated columns without row names, column names, or quotes around character strings.
-#' If the 'cool' type is selected, the .cool files are HDF5 containers that store Hi-C data, which will be read using `cooler2bedpe()` from the `HiCcompare` package.
-#'  
-#' 
-#' @examples
-#' \dontrun{
-#' datasets <- read_files("scHiCcompare/Example/Data/MG", "NSN", c(1, 2, 3), type = "txt")
-#' }
-#' @import HiCcompare
-#' @export
-
-
-
-read_files <- function(file.path, cell, position.dataset, type='txt'){
-  # Check if the file path exists
-  if(!dir.exists(file.path)){
-    stop("Error: The specified file path does not exist.")
-  }
-  
-  data_names <- list.files(path = file.path, full.names = TRUE, recursive = TRUE)[position.dataset] 
-  
-  # Check if the selected files are valid
-  if (length(data_names) == 0) {
-    stop("Error: No files found for the specified position.dataset.")
-  }
-  
-  datasets <- list()  # Initialize a list to store the datasets
-  
-  if (type == 'txt') {
-    for(i in 1:length(position.dataset)){
-      dataset <- tryCatch(
-        read.delim(data_names[i]),
-        error = function(e) stop(paste("Error reading file:", data_names[i]))
-      )
-      datasets[[i]] <- dataset  # Store each dataset in the list
-    }
-  } else if (type == 'cool') {
-    if (!requireNamespace("HiCcompare", quietly = TRUE)) {
-      stop("Error: HiCcompare package not installed. Please install the HiCcompare package.")
-    }
-    
-    library(HiCcompare)
-    for(i in 1:length(position.dataset)){
-      dataset <- tryCatch(
-        cooler2bedpe(data_names[i]),
-        error = function(e) stop(paste("Error reading .cool file:", data_names[i]))
-      )
-      datasets[[i]] <- dataset  # Store each dataset in the list
-    }
-  } else {
-    stop("Error: Unsupported file type. Use 'txt' or 'cool'.")
-  }
-  
-  return(datasets)
-}
-
-
-
-
 
 #' Create scHiC Interaction Frequency Table
 #'
 #' This function generates a single-cell Hi-C interaction frequency (IF) table for all single cells at a selected chromosome. The resulting table is usable for `Pooling_RF_impute` and `pseudo_bulkHic` functions. It reads the input files, extracts the relevant data, and outputs a table of interaction frequencies between genomic regions for each single cell.
 #'
-#' @param file.path The directory path where the data files are stored.
+#' @param file.list The list object where elements are processed scHi-C data file.
 #' @param cell.type The cell type used in the analysis (e.g., 'NSN', 'SN').
 #' @param position.dataset A vector of indices specifying the file positions to read from the directory.
-#' @param type The file type, either 'txt' or 'cool'. Default is 'txt'. If 'txt', the files should be in the format of a sparse upper triangular Hi-C matrix with five columns: chromosome 1 (chr1), start position 1 (start1), chromosome 2 (chr2), start position 2 (start2), and interaction frequency (IF). If 'cool', the .cool files will be read using `cooler2bedpe()` from the `HiCcompare` package.
 #' @param select.chromosome The chromosome name to be studied (e.g., 'chr1' or 'chrX').
-#' @return A data frame containing the interaction frequency table with genomic loci (cell, chromosome, start1, end1) and interaction frequencies (IF). This table can be used with `Pooling_RF_impute` and `pseudo_bulkHic` functions.
+#' @return A data frame containing the interaction frequency table with genomic loci (cell, chromosome, start1, end1) and interaction frequencies (IF) of each single cell. This table can be used with `Pooling_RF_impute` and `pseudo_bulkHic` functions.
 #' @details
-#' This function processes single-cell Hi-C data in '.txt' or 'cool' format, transforming them into a single 'scHiC table' data frame that is . Each input '.txt' file in the folder should be in the form of a sparse upper triangular Hi-C matrix with five tab-separated columns (chr1, start1, chr2, start2, IF) and no row or column names or quotes around character strings. If the 'cool' type is selected, the function uses `cooler2bedpe()` from the `HiCcompare` package to read the data.
+#' This function processes single-cell Hi-C data in a list object. Then the function transforms them into a single 'scHiC table' data frame. Each element in the list should be in the form of a sparse upper triangular Hi-C matrix with foyr tab-separated columns (chr, start1,  start2, IF) and no row or column names or quotes around character strings. 
 #' @examples
 #' \dontrun{
 #' # Load MG data folder example
 #' load_example_MGfolder()
+#' MGs_example = read_files(file.path = "MGs_example", cell = "MG", position.dataset = c(1, 2, 3), type = "txt", txt.sparse.heads.position = c(1,2,4,5))
 #' # Create scHiC table to be used in scHiCcompare
-#' IF_table <- scHiC_table(file.path = "MGs_example", cell.type = 'MG', position.dataset = 1:50, type = 'txt', select.chromosome = 'chr22')
+#' IF_table <- scHiC_table(file.list = MGs_example, cell.type = 'MG', position.dataset = 1:3, select.chromosome = 'chr22')
 #' }
 #' @import dplyr
 #' @export
 
 ## Create scHiC.table -----
-scHiC_table <- function(file.path, cell.type, position.dataset, type='txt', select.chromosome){
+scHiC_table <- function(file.list = NULL, cell.type, position.dataset, select.chromosome){
   # Input validation
-  if(missing(file.path) || missing(cell.type) || missing(position.dataset) || missing(select.chromosome)){
+  if(missing(cell.type) || missing(position.dataset) || missing(select.chromosome)){
     stop("Error: Missing one or more required arguments.")
   }
   
-  # Read data with error handling
-  datasets <- tryCatch(
-    read_files(file.path = file.path, cell = cell.type, position.dataset = position.dataset, type = type),
-    error = function(e) stop("Error in reading files: ", e$message)
-  )
-  
+  # Read data 
+  datasets = file.list[position.dataset]
   n_sc <- length(datasets)
   
   if (n_sc == 0) {
     stop("Error: No datasets available for processing.")
   }
+  
+  
   
   regions <- NULL
   options(scipen = 999)
@@ -127,10 +45,10 @@ scHiC_table <- function(file.path, cell.type, position.dataset, type='txt', sele
     data <- datasets[[i]]  # Get the dataset from the list
 
     # Select chromosome
-    data <- data[data[,1] == select.chromosome & data[,3] == select.chromosome,]
+    data <- data[data[,1] == select.chromosome,]
     
     # Transform dataset into sparse
-    data <- data[, c(2, 4, 5)]
+    data <- data[, -c(1)]
     names(data) <- c('region1', 'region2', 'IF')
     
     # Remove rows with 0 values in any column
@@ -173,13 +91,13 @@ scHiC_table <- function(file.path, cell.type, position.dataset, type='txt', sele
     data <- datasets[[i]]  # Get the dataset from the list
     
     # Filter rows based on chromosome
-    data <- data[data[,1] == select.chromosome & data[,3] == select.chromosome, ]
+    data <- data[data[,1] == select.chromosome & data[,3], ]
     if (nrow(data) == 0) {
       warning(paste("Warning: No data found for chromosome", select.chromosome, "in dataset", i))
       next
     }
     
-    data <- data[, c(2, 4, 5)]
+    data <- data[, -c(1)]
     names(data)[c(1, 2)] <- c('region1', 'region2')
     names(data)[3] <- paste0('IF_', i)
     data <- data[rowSums(data[,c(1,2)] == 0) == 0, ]
