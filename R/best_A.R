a8e3dc9b79b045b8349037ab05309e953016d93f
hamy12398 <hamy.12398@gmail.com>
2024-10-07 02:22:16 -0400

    update_package

---
+++ R/best_A.R
@@ -0,0 +1,81 @@
+##### Identify best A threshold in HiCcompare
+# Randomize_IFs - function to add noise to IFs of one matrix to create a similar 2 matrix
+randomize_IFs <- function(hic.table, SD) {
+  # copy first IF vector
+  newIF2 <- hic.table$IF1
+  # add constant offset
+  newIF2 <- newIF2 + 5
+  # add random noise
+  newIF2 <- newIF2 + rnorm(length(newIF2), 0, SD)
+  # check for 0's and negatives
+  newIF2[newIF2 <= 0] <- 1
+  # create new hic.table with new IF vectors
+  sparse1 <- cbind(hic.table$start1, hic.table$start2, hic.table$IF1)
+  sparse2 <- cbind(hic.table$start1, hic.table$start2, newIF2)
+  temp.table <- create.hic.table(sparse1, sparse2, chr = hic.table$chr1[1])
+  return(temp.table)
+}
+
+
+### function to find best A
+best_A <- function (hic.table, SD = 2, numChanges = 35, FC = 3, alpha = 0.05, 
+                    Plot = FALSE) 
+{
+  if (is(hic.table, "list")) {
+    stop("Enter a single hic.table object, not a list of hic.tables")
+  }
+  new.table <- randomize_IFs(hic.table, SD)
+  new.table <- new.table[abs(new.table$M) < 2, ]
+  sample_space <- 1:nrow(new.table)
+  tmp_A <- (new.table$IF1 + new.table$IF2)/2
+  low_A <- which(tmp_A < quantile(tmp_A, 0.1))
+  changes <- sample(sample_space[-low_A], numChanges)
+  meanIF <- ((new.table[changes, ]$IF1 + new.table[changes, 
+  ]$IF2)/2) %>% round() %>% as.integer()
+  suppressWarnings(new.table[changes, `:=`(IF1, meanIF)])
+  suppressWarnings(new.table[changes, `:=`(IF2, meanIF)])
+  midpoint <- floor(numChanges/2)
+  newIF1 <- new.table[changes[1:midpoint], ]$IF1 * FC %>% as.integer()
+  newIF2 <- new.table[changes[(midpoint + 1):numChanges], ]$IF2 * 
+    FC %>% as.integer()
+  new.table[changes[1:midpoint], `:=`(IF1, newIF1)]
+  new.table[changes[(midpoint + 1):numChanges], `:=`(IF2, newIF2)]
+  new.table = new.table[, `:=`(M, log2(IF2/IF1))]
+  truth <- rep(0, nrow(new.table))
+  truth[changes] <- 1
+  new.table[, `:=`(truth, truth)]
+  new.table <- hic_loess(new.table, Plot = Plot)
+  new.table <- suppressMessages(hic_compare(new.table, Plot = Plot))
+  TP <- vector(length = 50)
+  FP <- vector(length = 50)
+  FN <- vector(length = 50)
+  TN <- vector(length = 50)
+  A_seq <- seq(1, 50, by = 1)
+  for (i in seq_along(A_seq)) {
+    tmp.table <- suppressMessages(hic_compare(new.table, 
+                                              A.min = A_seq[i], adjust.dist = TRUE, p.method = "fdr", 
+                                              Plot = FALSE))
+    TP[i] <- sum(tmp.table$p.adj < alpha & tmp.table$truth == 
+                   1)
+    FP[i] <- sum(tmp.table$p.adj < alpha & tmp.table$truth == 
+                   0)
+    FN[i] <- sum(tmp.table$p.adj >= alpha & tmp.table$truth == 
+                   1)
+    TN[i] <- sum(tmp.table$p.adj >= alpha & tmp.table$truth == 
+                   0)
+  }
+  MCC <- ((TP * TN) - (FP * FN))/(sqrt((TP + FP)) * sqrt((TP + 
+                                                            FN)) * sqrt((TN + FP)) * sqrt((TN + FN)))
+  FPR <- FP/(FP + TP)
+  FNR <- FN/(FN + TN)
+  TPR <- TP/(TP + FP)
+  
+  #Best A in term of MCC, TPR, FPR
+  MCC.A = which(MCC == max(MCC))
+  TPR.A = which(TPR == max(TPR))
+  FPR.A = which(FPR == min(FPR))
+  intersect.MCC_TPR.A <- intersect(MCC.A,TPR.A)
+  best_A <- intersect(intersect.MCC_TPR.A, FPR.A)[1]
+  return(best_A)
+}
+
